b0ae34beba0c4531d46d8a84794bfb70a61f2bd5
Vincent Nahn <vincentnahn@gmail.com>
2026-04-11 00:15:39 +0200

    feat: Add better auth base url

--- templates/tanstack/src/lib/auth.ts
+++ templates/tanstack/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const auth = betterAuth({
+	baseURL: "http://127.0.0.1:3000",
 	emailAndPassword: {
 		enabled: true,
 	},
